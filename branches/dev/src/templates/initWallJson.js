621ec231eb88813e05aeeca6634134923f14f614
yilmazhuseyin <yilmazhuseyin@099abfc2-aa8b-11de-a118-c7387b1e4f66>
2010-06-20 20:46:18 +0000

    copy trunk to dev branch

---
+++ branches/dev/src/templates/initWallJson.js
@@ -0,0 +1,10 @@
+{caller:{{callbackObject}},initFunction:function()
+{ this.pageSize={{wall.pageSize}};
+this.nickLabel="{{wall.nickLabel}}";
+this.nick2Label="{{wall.nick2Label}}";
+this.postAreaLabel="{{wall.postAreaLabel}}";
+this.postButtonLabel="{{wall.postButtonLabel}}";
+this.resetButtonLabel="{{wall.resetButtonLabel}}";
+this.formWidth="{{wall.formWidth}}";
+this.formHeight="{{wall.formHeight}}";
+this.wallStyle=1;}}
